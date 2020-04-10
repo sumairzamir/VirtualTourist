31a3077d27838ef1bee2a703cb753f0dd9246bf6
Sumair Zamir <sumair.zamir@gmail.com>
2020-04-10 16:28:50 +0100

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Aiman Nabeel on 10/04/2020.
+//  Copyright © 2020 Sumair Zamir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
